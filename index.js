9dfb41f16e6d5c0423beaf42311468283b5d200e
Diego Garcia <diego.ds.garcia@gmail.com>
2020-08-11 11:04:52 -0300

    add handlers of notification and remove Modal

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from 'expo'
+import messaging from '@react-native-firebase/messaging'
 
-import App from './src/App';
+import App from './src/App'
+
+messaging().setBackgroundMessageHandler(async () => {
+  console.log('Ta acontecendo alguma coisa em background')
+})
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(App)
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,50 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Alert, View } from 'react-native'
+import { StyleSheet, View, Alert } from 'react-native'
 
 import messaging from '@react-native-firebase/messaging'
-import { requestUserPermission } from './service/notification'
+import { requestUserPermission, getToken } from './service/notification'
 
 import Navi from './components/Navi'
+import Text from './components/Text'
 import Button from './components/Button'
 
 export default function App() {
+  const [token, setToken] = useState<string>('Listen...')
+  const [openModal, setOpenModal] = useState(false)
+
+  const onReceiveMessage = () => {
+    return messaging().onMessage(remoteMessage => {
+      Alert.alert('Chegou uma nova mensagem!', JSON.stringify(remoteMessage));
+    })
+  }
+
+  const onReceiveInBackground = () => {
+    return messaging().onNotificationOpenedApp(remoteMessage => {
+      Alert.alert('Chegou uma nova mensagem enquanto o App tava fechado!', JSON.stringify(remoteMessage));
+    })
+  }
+
   useEffect(() => {
     requestUserPermission()
-
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    })  
-
-    return unsubscribe
+    getToken().then(setToken)
+    const unsubscribeReceive = onReceiveMessage()
+    const unsubscribeReceiveBackground = onReceiveInBackground()
+    
+    return () => {
+      unsubscribeReceive()
+      unsubscribeReceiveBackground()
+    }
   }, [])
+
   return (
     <View style={styles.container}>
       <Navi />
       <StatusBar style="auto" />
-      <Button>Heey!</Button>
+      <Button onPress={() => setOpenModal(true)}>
+        Heey!
+      </Button>
+      <Text>{token}</Text>
     </View>
   )
 }

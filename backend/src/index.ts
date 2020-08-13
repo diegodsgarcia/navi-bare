9cfd3fc5e613b422875061fd5cba57980b791666
Diego Garcia <diego.ds.garcia@gmail.com>
2020-08-13 11:27:34 -0300

    update backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,6 +24,7 @@ app.post('/send', async (req, res) => {
     notification: {
       title,
       body,
+      image: 'https://i.pinimg.com/originals/f8/27/ed/f827ed9a704146f65b96226f430abf3c.png'
     }
   }
 
@@ -34,6 +35,23 @@ app.post('/send', async (req, res) => {
     return res.status(500).send(error)
   }
 })
+
+app.get('/sendAll', async (req, res) => {
+  const result = await admin.messaging().sendMulticast({
+    tokens: [
+      'e6oGLip3TLeWiomtFjRGRB:APA91bGx2_1fV4r4qa_1i1gq8W_qGX6OGMv7ZbPzH3Qy861iKm-b-YJ0C4F4E2RxFhbvVvIIBVJX_4VI95pIiudnQm0vMpsdEwI991G4SlhENDBKPD3UHCnnknkOnqP37fkMuO8tPlm0',
+      'eSCu45rXTjqfmkNlfHX0zL:APA91bFUGJfcIjpu5dFChRXB8oB8w569gZYeY7Gr-vwLScQyAeg-IdSLqRKlDI-6E1M9AW0uHQ0DDQCuv6otf8Pg6FzWRPsILkVC3LvEQU29huPUbBwHOdZsuuYkLFgCeyk-qfRWhaRC',
+    ],
+    notification: {
+      title: 'Test',
+      body: 'aaaaaaaaaaaaaaa',
+      imageUrl: 'https://i.pinimg.com/originals/f8/27/ed/f827ed9a704146f65b96226f430abf3c.png'
+    }
+  })
+
+  return res.json(result)
+})
+
 
 app.listen(9000, () => {
   console.log('Server run in 9000')

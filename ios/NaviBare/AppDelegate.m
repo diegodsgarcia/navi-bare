387bde82f732d17122d82821c0dbd3b1622e7a88
Diego Garcia <diego.ds.garcia@gmail.com>
2020-09-11 10:57:49 -0300

    add config of ios to notification

--- ios/NaviBare/AppDelegate.m
+++ ios/NaviBare/AppDelegate.m
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+#import <Firebase.h>
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -40,9 +42,13 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
-  InitializeFlipper(application);
-#endif
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
+  #if DEBUG
+    InitializeFlipper(application);
+  #endif
   
   self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
   self.launchOptions = launchOptions;
